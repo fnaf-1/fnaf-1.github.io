9e7048394ee4c8c12a138cac88d1fd93b0baeb7f
Haleyschool <124913580+Haleyschool@users.noreply.github.com>
2025-03-13 23:41:59 +0700

    7

---
+++ js/games-data.js
@@ -0,0 +1,23 @@
+const gameList = [
+  {
+    name: "Johnny Revenge",
+    url: "/game/johnny-revenge.html",
+    image: "https://slope-game.github.io/johnny-revenge/logo.png",
+  },
+  {
+    name: "Skateboard Racing",
+    url: "/game/skateboard-racing.html",
+    image: "https://bitlifeonline.github.io/skateboard-racing/logo.png",
+  },
+  {
+    name: "Raft Life",
+    url: "/game/raft-life.html",
+    image: "https://bitlifeonline.github.io/raft-life/logo.png",
+  },
+  {
+    name: "Drift Dudes",
+    url: "/game/drift-dudes.html",
+    image: "https://ubg77.github.io/drift-dudes/logo.png",
+  },
+  // Thêm các game khác của bạn vào đây theo cùng format
+];
